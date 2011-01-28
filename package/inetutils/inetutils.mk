c019a3596f48cd022cda2c355106bf2a85674de9
Bob Liu <lliubbo@gmail.com>
2011-01-28 17:30:54 +0800

    buildroot:package: add inetutils
    Add inetutils to buildroot.
    
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

---
+++ package/inetutils/inetutils.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# bluez_utils
+#
+#############################################################
+INETUTILS_VERSION = 1.8
+INETUTILS_SOURCE = inetutils-$(INETUTILS_VERSION).tar.gz
+INETUTILS_SITE = http://ftp.gnu.org/gnu/inetutils/
+INETUTILS_AUTORECONF = NO
+INETUTILS_INSTALL_STAGING = NO
+INETUTILS_INSTALL_TARGET = YES
+INETUTILS_CONF_OPT = \
+	--disable-ifconfig \
+
+$(eval $(call AUTOTARGETS,package,inetutils))
