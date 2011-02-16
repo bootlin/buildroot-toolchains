cfd384415f88526d8dc5b6b263c0c22411ea1d8a
Sonic Zhang <sonic.zhang@analog.com>
2011-02-16 14:58:14 +0800

    buildroot:inetutils: Set SUIDMODE to INSTALL_TARGET_OPT instead of patch Makefile.
    Clean up redundant Macro as well.
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/inetutils/inetutils.mk
+++ package/inetutils/inetutils.mk
@@ -6,10 +6,9 @@
 INETUTILS_VERSION = 1.8
 INETUTILS_SOURCE = inetutils-$(INETUTILS_VERSION).tar.gz
 INETUTILS_SITE = http://ftp.gnu.org/gnu/inetutils/
-INETUTILS_AUTORECONF = NO
 INETUTILS_INSTALL_STAGING = NO
-INETUTILS_INSTALL_TARGET = YES
+INETUTILS_INSTALL_TARGET_OPT = SUIDMODE="-m 4775" DESTDIR=$(TARGET_DIR) install
 INETUTILS_CONF_OPT = \
-	--disable-ifconfig \
+	--disable-ifconfig
 
 $(eval $(call AUTOTARGETS,package,inetutils))
