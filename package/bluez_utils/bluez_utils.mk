4c3380d9dbb02428ed97a4a0530469192001aca1
Bob Liu <lliubbo@gmail.com>
2011-02-21 11:40:58 +0800

    buildroot:package: update bluez utils
    Update bluez utils to ver 4.87 and cleanup bluez_utils.mk.
    
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- package/bluez_utils/bluez_utils.mk
+++ package/bluez_utils/bluez_utils.mk
@@ -3,13 +3,12 @@
 # bluez_utils
 #
 #############################################################
-BLUEZ_UTILS_VERSION = 4.86
+BLUEZ_UTILS_VERSION = 4.87
 BLUEZ_UTILS_SOURCE = bluez-$(BLUEZ_UTILS_VERSION).tar.gz
 BLUEZ_UTILS_SITE = http://www.kernel.org/pub/linux/bluetooth/
-BLUEZ_UTILS_AUTORECONF = NO
-BLUEZ_UTILS_INSTALL_STAGING = NO
+BLUEZ_UTILS_INSTALL_STAGING = YES
 BLUEZ_UTILS_INSTALL_TARGET = YES
 BLUEZ_UTILS_DEPENDENCIES += dbus libglib2
-BLUEZ_UTILS_CONF_OPT = \
+BLUEZ_UTILS_CONF_OPT = --enable-hidd
 
 $(eval $(call AUTOTARGETS,package,bluez_utils))
