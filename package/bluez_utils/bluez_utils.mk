39d6ff14373d65d7b190f58e4ea6b18efe1b4dc7
Bob Liu <lliubbo@gmail.com>
2011-04-19 10:31:57 +0800

    buildroot:bluez: enable test
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- package/bluez_utils/bluez_utils.mk
+++ package/bluez_utils/bluez_utils.mk
@@ -9,6 +9,6 @@ BLUEZ_UTILS_SITE = http://www.kernel.org/pub/linux/bluetooth/
 BLUEZ_UTILS_INSTALL_STAGING = YES
 BLUEZ_UTILS_INSTALL_TARGET = YES
 BLUEZ_UTILS_DEPENDENCIES += dbus libglib2
-BLUEZ_UTILS_CONF_OPT = --enable-hidd
+BLUEZ_UTILS_CONF_OPT = --enable-hidd --enable-test
 
 $(eval $(call AUTOTARGETS,package,bluez_utils))
