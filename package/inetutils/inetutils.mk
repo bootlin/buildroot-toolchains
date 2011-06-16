dc8857da5ac6e263b8bf897c146a8c7f1bcc4f0b
Bob Liu <lliubbo@gmail.com>
2011-06-16 17:41:30 +0800

    buildroot:devonshire: add inetutils-1.6 support
    Since there is some compile err while using inetuitl-1.8 on blackfin, rollback
    to use 1.6 and added it into buildroot.
    
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- package/inetutils/inetutils.mk
+++ package/inetutils/inetutils.mk
@@ -3,12 +3,17 @@
 # inet_utils
 #
 #############################################################
-INETUTILS_VERSION = 1.8
+INETUTILS_VERSION = 1.6
 INETUTILS_SOURCE = inetutils-$(INETUTILS_VERSION).tar.gz
 INETUTILS_SITE = http://ftp.gnu.org/gnu/inetutils/
 INETUTILS_INSTALL_STAGING = NO
 INETUTILS_INSTALL_TARGET_OPT = SUIDMODE="-m 4775" DESTDIR=$(TARGET_DIR) install
 INETUTILS_CONF_OPT = \
-	--disable-ifconfig
-
+		     --disable-ifconfig \
+		     --without-included-regex \
+		     --disable-libls \
+		     --disable-servers --disable-clients \
+		     --enable-rcp \
+		     --enable-rsh \
+		     --enable-rshd
 $(eval $(call AUTOTARGETS,package,inetutils))
