5793bc27e941be44541749e10af269f636880620
Bob Liu <lliubbo@gmail.com>
2011-06-22 13:55:51 +0800

    buildroot:package: add nbench
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

---
+++ package/nbench/nbench.mk
@@ -0,0 +1,31 @@
+#############################################################
+#
+# nbench
+#
+#############################################################
+NBENCH_VERSION:=2.2.3
+NBENCH_SOURCE:=nbench-byte-$(NBENCH_VERSION).tar.gz
+NBENCH_SITE:=http://www.tux.org/~mayer/linux/
+
+define NBENCH_CONFIGURE_CMDS
+	$(CONFIG_UPDATE) $(@D)
+	sed -i 's/CFLAGS=/CFLAGS+=/g' $(@D)/Makefile
+	sed -i '/cd .*doc/d' $(@D)/Makefile
+	sed -i '/include/d' $(@D)/Makefile
+	touch $@
+endef
+
+define NBENCH_BUILD_CMDS
+	$(MAKE) CFLAGS="$(TARGET_CFLAGS)" OS=$(ARCH) CC="$(TARGET_CC)" -C $(@D)
+endef
+
+define NBENCH_INSTALL_TARGET_CMDS
+	$(MAKE) CFLAGS="$(TARGET_CFLAGS)" OS=$(ARCH) CC="$(TARGET_CC)" BASE=$(TARGET_DIR) -C $(@D) install
+endef
+
+define NBENCH_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(call AUTOTARGETS,package,nbench))
+
