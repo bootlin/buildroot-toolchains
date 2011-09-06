d2351cd96e22ca7cbc131419813c5563374face6
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-09-06 18:13:45 +0200

    getinge: add separate partition for the configuration
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/getinge/netcom/post-build.sh
+++ board/getinge/netcom/post-build.sh
@@ -9,3 +9,9 @@ fi
 
 # Copy the rootfs additions
 cp -a $BOARDDIR/rootfs-additions/* $TARGETDIR/
+
+# Generate the factory default image
+mkfs.jffs2 -p -e 128 -o $TARGETDIR/usr/share/conf.d/defaults.img -d $BOARDDIR/conf.d/
+
+# Copy the configuration version
+cp $BOARDDIR/conf.d/version $TARGETDIR/usr/share/conf.d/
