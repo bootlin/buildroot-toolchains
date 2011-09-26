8770f71337be6d430e3ca1ffd318bcc64dd4e965
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-09-26 15:11:46 +0200

    netcom: generate an U-Boot environment image
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/getinge/netcom/samba-script.tcl
+++ board/getinge/netcom/samba-script.tcl
@@ -1,4 +1,5 @@
 DATAFLASH::Init 0
 DATAFLASH::EraseAll
 GENERIC::SendFile "output/images/dataflash_at91sam9m10g45ek.bin" 0x0 1
+send_file {DataFlash AT45DB/DCB} "output/images/u-boot-env.bin" 0x4200 0
 send_file {DataFlash AT45DB/DCB} "output/images/u-boot.bin" 0x8400 0
