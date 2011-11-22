5226fcf315de4c52106cc903a3922a67ec777641
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-11-22 17:38:46 +0100

    netcom-cgic: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/getinge/netcom-cgic/netcom-cgic.mk
@@ -0,0 +1,18 @@
+NETCOM_CGIC_SOURCE =
+NETCOM_CGIC_INSTALL_STAGING = YES
+
+define NETCOM_CGIC_EXTRACT_CMDS
+	cp -a $(GETINGE_LIBS_TOPDIR)/libcgic_201/* $(@D)/
+endef
+
+define NETCOM_CGIC_BUILD_CMDS
+	$(MAKE) -C $(@D) BUILDROOT=1 clean
+	$(MAKE) -C $(@D) BUILDROOT=1 CC="$(TARGET_CC)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)"
+endef
+
+define NETCOM_CGIC_INSTALL_STAGING_CMDS
+	install -D -m 0644 $(@D)/cgic.h $(STAGING_DIR)/usr/include/
+	install -D -m 0644 $(@D)/libcgic.a $(STAGING_DIR)/usr/lib/
+endef
+
+$(eval $(call GENTARGETS,package/getinge,netcom-cgic))
