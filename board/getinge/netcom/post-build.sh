469d18e615c77ec79ec34d79a31ea887d87075cd
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-11-22 17:13:08 +0100

    persistent-files.conf: change strategy
    Instead of having persistent-files.conf in the rootfs-additions, just
    add some lines to this file from the post-build.sh script. The
    advantage is that we don't override what package can have added to
    this file.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/getinge/netcom/post-build.sh
+++ board/getinge/netcom/post-build.sh
@@ -7,5 +7,11 @@ if ! grep -q ttyS4 $TARGETDIR/etc/securetty ; then
 	echo "ttyS4" >> $TARGETDIR/etc/securetty ;
 fi
 
+# Add some generic lines to the persistent-files.conf file
+grep -q "/etc/conf.d/net.eth*" $TARGETDIR/etc/persistent-files.conf || \
+    echo "/etc/conf.d/net.eth*" >> $TARGETDIR/etc/persistent-files.conf
+grep -q "/etc/conf.d/hostname" $TARGETDIR/etc/persistent-files.conf || \
+    echo "/etc/conf.d/hostname" >> $TARGETDIR/etc/persistent-files.conf
+
 # Copy the rootfs additions
 cp -a $BOARDDIR/rootfs-additions/* $TARGETDIR/
