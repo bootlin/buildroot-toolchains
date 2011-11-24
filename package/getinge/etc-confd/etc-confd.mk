86002574324ce2b544a7162211f89dab39042b9e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-11-24 17:04:09 +0100

    etc-confd: bundle the config files from BINARIES_DIR/etc/conf.d
    Until now, this package was creating a JFFS2 filesystem from the
    contents of board/getinge/netcom/conf.d/. However, this doesn't take
    into account the configuration files installed by other packages such
    as netcom-apps. Therefore, we now bundle the contents of
    $(BINARIES_DIR)/conf.d/. The contents of board/getinge/netcom/conf.d/
    is copied there before the mkfs.jffs2 command is used.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/getinge/etc-confd/etc-confd.mk
+++ package/getinge/etc-confd/etc-confd.mk
@@ -2,16 +2,16 @@
 ETC_CONFD_SOURCE  =
 ETC_CONFD_VERSION = 1.0
 
-ETC_CONFD_DEPENDENCIES = host-mtd
+ETC_CONFD_DEPENDENCIES = host-mtd netcom-apps
 
 define ETC_CONFD_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/conf.d/
+	cp -a board/getinge/netcom/conf.d/* $(BINARIES_DIR)/conf.d/
 	$(HOST_DIR)/usr/sbin/mkfs.jffs2 -n -p -e 128 \
 		-o $(TARGET_DIR)/usr/share/conf.d/defaults.img \
-		-d board/getinge/netcom/conf.d/
+		-d $(BINARIES_DIR)/conf.d/
 	install -D -m 0644 board/getinge/netcom/conf.d/version \
 		$(TARGET_DIR)/usr/share/conf.d/version
 endef
 
 $(eval $(call GENTARGETS,package/getinge,etc-confd))
-
