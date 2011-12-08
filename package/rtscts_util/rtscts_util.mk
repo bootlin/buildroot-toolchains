4a5c8b2ac7f8ce31300ca8b6ba6e864497041b8c
Sonic Zhang <sonic.zhang@analog.com>
2011-12-08 18:44:59 +0800

    buildroot: packages: Add uart flow control test utility.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

---
+++ package/rtscts_util/rtscts_util.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# rtscts test util
+#
+#############################################################
+RTSCTS_UTIL_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-test/rtscts_test
+RTSCTS_UTIL_SITE_METHOD = svn
+RTSCTS_UTIL_VERSION = 10459
+
+define RTSCTS_UTIL_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define RTSCTS_UTIL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/rtscts_test $(TARGET_DIR)/bin/
+endef
+
+define RTSCTS_UTIL_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define RTSCTS_UTIL_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/bin/rtscts_test
+endef
+
+$(eval $(call GENTARGETS,package,rtscts_util))
+
