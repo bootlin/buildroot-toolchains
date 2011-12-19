0c3cc10c6cee322cc62237b9e8ee5d3720891e2b
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-12-19 17:58:41 +0100

    netcom: set correct default passwords
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/getinge/netcom/post-build.sh
+++ board/getinge/netcom/post-build.sh
@@ -7,6 +7,13 @@ if ! grep -q ttyS4 $TARGETDIR/etc/securetty ; then
 	echo "ttyS4" >> $TARGETDIR/etc/securetty ;
 fi
 
+# Disable login with the 'default' account
+sed -i 's/^default::/default:*:/' $TARGETDIR/etc/shadow
+
+# Set up the default root password. Generated with the 'mkpasswd'
+# utility.
+sed -i 's/^root::/root:GlUwvv8.UEkdE:/' $TARGETDIR/etc/shadow
+
 # Add some generic lines to the persistent-files.conf file
 grep -q "/etc/conf.d/net.eth*" $TARGETDIR/etc/persistent-files.conf || \
     echo "/etc/conf.d/net.eth*" >> $TARGETDIR/etc/persistent-files.conf
