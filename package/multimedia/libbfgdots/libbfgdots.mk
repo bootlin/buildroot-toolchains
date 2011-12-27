119baca455d12623a8be3247bf203808e2d5d45f
Sonic Zhang <sonic.zhang@analog.com>
2011-12-27 18:13:08 +0800

    buildroot:libbfgdots: Add blackfin g.729 codec library.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

---
+++ package/multimedia/libbfgdots/libbfgdots.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# libbfgdots
+#
+#############################################################
+LIBBFGDOTS_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/lib/libbfgdots/g729
+LIBBFGDOTS_SITE_METHOD = svn
+LIBBFGDOTS_VERSION = 10459
+LIBBFGDOTS_INSTALL_STAGING = YES
+LIBBFGDOTS_INSTALL_TARGET = YES
+
+define LIBBFGDOTS_BUILD_CMDS
+	$(MAKE) -C $(@D)
+endef
+
+define LIBBFGDOTS_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) CONFIG_FMT_USE_FDPIC_ELF=$(BR2_BFIN_FDPIC) install
+endef
+
+define LIBBFGDOTS_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) CONFIG_FMT_USE_FDPIC_ELF=$(BR2_BFIN_FDPIC) install
+endef
+
+define LIBBFGDOTS_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(call GENTARGETS,package,libbfgdots))
+
