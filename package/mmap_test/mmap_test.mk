0c0da59be4e9938aa5b5d8d4d86f3555dcfb4e37
Sonic Zhang <sonic.zhang@analog.com>
2012-03-09 17:30:40 +0800

    package: mmap: change to SVN head
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/mmap_test/mmap_test.mk
+++ package/mmap_test/mmap_test.mk
@@ -5,7 +5,7 @@
 #############################################################
 MMAP_TEST_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-test/mmap_test
 MMAP_TEST_SITE_METHOD = svn
-MMAP_TEST_VERSION = 10630
+MMAP_TEST_VERSION = HEAD
 
 define MMAP_TEST_BUILD_CMDS
 	$(MAKE) -C $(@D) BASE_DIR=$(BASE_DIR) MMAP_MODULE_DIR=$(BASE_DIR)/build/mmap_test-$(MMAP_TEST_VERSION) $(TARGET_CONFIGURE_OPTS)
