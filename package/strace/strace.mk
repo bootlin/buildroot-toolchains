729560150e72dd929083bf266166a84c6cb20d77
Steven Miao <realmz6@gmail.com>
2012-03-19 16:30:44 +0800

    buildroot: add strace support, update to strace 4.6
    Signed-off-by: Steven Miao <realmz6@gmail.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,13 +4,25 @@
 #
 #############################################################
 
-STRACE_VERSION = 4.5.20
-STRACE_SOURCE = strace-$(STRACE_VERSION).tar.bz2
-STRACE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/strace
+STRACE_VERSION = HEAD
+STRACE_SITE_METHOD = svn
+STRACE_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/strace
+
+FLTFLAGS += -s 0x5000
 
 STRACE_CONF_ENV = ac_cv_header_linux_if_packet_h=yes \
 		  ac_cv_header_linux_netlink_h=yes \
-	          $(if $(BR2_LARGEFILE),ac_cv_type_stat64=yes,ac_cv_type_stat64=no)
+	          ac_cv_type_stat64=no \
+		  ac_cv_have_long_long_off_t=no
+
+STRACE_SUBDIR = strace-4.6
+
+define STRACE_CHECK_ENTITIES 
+	cd $(@D) && \
+	./check-entities.sh $(TOPDIR)/$(LINUX26_SOURCE_DIR) $(KERNEL_ARCH) $(STRACE_SUBDIR) $(ARCH)
+endef
+STRACE_PRE_CONFIGURE_HOOKS += STRACE_CHECK_ENTITIES
+
 
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
