ddc2786ea34e30148c6704f15e3cc4ba01e8dc52
Gabriel Siftar <gabe.siftar@getingeusa.com>
2012-03-19 15:35:42 -0400

    Updated for new dataflash layout.  Added reboot following programming

--- board/getinge/netcom/samba-script.tcl
+++ board/getinge/netcom/samba-script.tcl
@@ -1,5 +1,12 @@
 DATAFLASH::Init 0
 DATAFLASH::EraseAll
 GENERIC::SendFile "output/images/dataflash_at91sam9m10g45ek.bin" 0x0 1
-send_file {DataFlash AT45DB/DCB} "output/images/u-boot-env.bin" 0x4200 0
-send_file {DataFlash AT45DB/DCB} "output/images/u-boot.bin" 0x8400 0
+send_file {DataFlash AT45DB/DCB} "output/images/u-boot-env.bin" 0x2100 0
+send_file {DataFlash AT45DB/DCB} "output/images/u-boot.bin" 0x4200 0
+send_file {DataFlash AT45DB/DCB} "output/images/uboot.script.img" 0x37B00 0
+send_file {DataFlash AT45DB/DCB} "output/images/u-boot-env.bin" 0x3DE00 0
+
+after 200;
+TCL_Write_Int $target(handle) 0xA500000D 0xFFFFFD00; # Reset!
+
+
