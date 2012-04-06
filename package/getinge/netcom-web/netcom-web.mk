6e00a0a64a135334494e6466d8eea33f8fa7fb3c
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-04-06 14:57:22 +0200

    netcom-web: ensure .cgi scripts are executable
    When the netcom-web source code is read from a Samba share or another
    filesystem that does not support the Unix traditional permissions,
    .cgi scripts were installed in the target filesystem without execution
    permissions. This commit ensures that such .cgi scripts (and the
    auxilliary 'functions' file) have execution permissions.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/getinge/netcom-web/netcom-web.mk
+++ package/getinge/netcom-web/netcom-web.mk
@@ -9,6 +9,8 @@ endef
 define NETCOM_WEB_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/html/
 	cp -a $(@D)/* $(TARGET_DIR)/usr/share/html/
+	find $(TARGET_DIR)/usr/share/html/cgi-bin/ -name '*.cgi' | xargs chmod +x
+	chmod +x $(TARGET_DIR)/usr/share/html/cgi-bin/functions
 	ln -sf /usr/bin/fwupgrade-cgi $(TARGET_DIR)/usr/share/html/cgi-bin/fwupgrade-cgi
 	install -D -m 0755 $($(PKG)_DIR_PREFIX)/netcom-web/S98httpd $(TARGET_DIR)/etc/init.d/S98httpd
 	install -D -m 0644 $($(PKG)_DIR_PREFIX)/netcom-web/httpd.conf $(TARGET_DIR)/etc/httpd.conf
