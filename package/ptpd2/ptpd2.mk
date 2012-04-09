3118e694e7e77103348302afc6ef18b5028224c0
Sonic Zhang <sonic.zhang@analog.com>
2012-04-09 15:46:55 +0800

    buildroot: ptpd2: Add ptp daemon for IEEE1588 v2.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

---
+++ package/ptpd2/ptpd2.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# ptpd2
+#
+#############################################################
+PTPD2_VERSION = HEAD
+PTPD2_SITE = https://github.com/richardcochran/ptpd-phc.git
+PTPD2_SITE_METHOD = git
+
+define PTPD2_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS) -lm -lrt" -C $(@D)/src
+endef
+
+define PTPD2_INSTALL_TARGET_CMDS
+	cp -dpf $(@D)/src/ptpd2 $(TARGET_DIR)/usr/sbin
+endef
+
+$(eval $(call GENTARGETS,package,ptpd2))
