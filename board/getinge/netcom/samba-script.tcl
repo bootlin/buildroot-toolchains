e59e751dbd464d9769fec2e3eef6c44a9530a233
Gabriel Siftar <gabe.siftar@getingeusa.com>
2012-04-10 13:10:52 -0400

    Updated to set reset mode and then reset board following programming

--- board/getinge/netcom/samba-script.tcl
+++ board/getinge/netcom/samba-script.tcl
@@ -7,6 +7,8 @@ send_file {DataFlash AT45DB/DCB} "output/images/uboot.script.img" 0x37B00 0
 send_file {DataFlash AT45DB/DCB} "output/images/u-boot-env.bin" 0x3DE00 0
 
 after 200;
+TCL_Write_Int $target(handle) 0xA5000000 0xFFFFFD08; # Set reset mode
+after 200;
 TCL_Write_Int $target(handle) 0xA500000D 0xFFFFFD00; # Reset!
-
+after 2000;
 
