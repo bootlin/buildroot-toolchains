9ed6f57cba2ff76583528cf1b230197812e0eace
Gabriel Siftar <gabe.siftar@getingeusa.com>
2012-04-30 09:27:58 -0400

    Added installation of needed perl components

--- board/getinge/netcom/prodtest/files/scripts/preupdate.pl
+++ board/getinge/netcom/prodtest/files/scripts/preupdate.pl
@@ -6,10 +6,26 @@ my $scriptdir = "/opt/getinge/";
 
 # Insert code here that should run before extracting the archive
 
+# addLineToFile line, filename
+#  adds line to filename if it's not already present
+sub addLineToFile {
+  `grep "$_[0]" "$_[1]"`;
+  if ($? != 0) {
+    `echo "$_[0]" >> "$_[1]"`;
+  }
+}
+
 #Clean up before applying new version
 remove_tree("${scriptdir}prod-def");
 
 #Install needed perl modules
-`cpan Config::Simple` or die "Can't install perl modules: $!\n";
+#`cpan Config::Simple` or die "Can't install perl modules: $!\n";
+
+
+# install needed perl modules as packages
+addLineToFile("deb http://debian.pkgs.cpan.org/debian unstable main","/etc/apt/sources.list");
+`apt-get update`;
+`apt-cache search libtest-harness-perl`;
+`apt-get install libconfig-simple-perl`;
 
 exit(0);
