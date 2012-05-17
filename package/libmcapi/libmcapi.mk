84f5b67a431ad7ea543fc0131afe20414bc5d89b
Steven Miao <realmz6@gmail.com>
2012-05-17 18:10:38 +0800

    [#6984] add icc utils and libmcapi to buildroot
    Signed-off-by: Steven Miao <realmz6@gmail.com>

---
+++ package/libmcapi/libmcapi.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# libmcapi
+#
+#############################################################
+
+LIBMCAPI_VERSION = HEAD
+LIBMCAPI_SITE_METHOD = svn
+LIBMCAPI_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/lib/libmcapi
+
+LIBMCAPI_SUBDIR = libmcapi-2.0
+LIBMCAPI_INSTALL_STAGING = YES
+
+KERNEL_DIR = $(TOPDIR)/$(LINUX26_SOURCE_DIR)
+ifeq ($(BR2_PACKAGE_ICC_CPU_BF609),y)
+ICC_MACHINE=bf609
+else
+ICC_MACHINE=bf561
+endif
+
+ICC_INCLUDE="CFLAGS=-I$(KERNEL_DIR)/drivers/staging/icc/include -I${KERNEL_DIR}/arch/blackfin/include"
+
+LIBMCAPI_CONF_OPT+=$(ICC_INCLUDE)
+
+$(eval $(call AUTOTARGETS,package,libmcapi))
