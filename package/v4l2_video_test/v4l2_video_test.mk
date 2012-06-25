d09cee4588549dbcaaa4e13d64abd1f4211ef018
Vivi Li <vivi.li@analog.com>
2012-06-25 17:36:14 +0800

    package: v4l2_video_test: fix typo in v4l2_video_test
    Signed-off-by: Vivi Li <vivi.li@analog.com>

--- package/v4l2_video_test/v4l2_video_test.mk
+++ package/v4l2_video_test/v4l2_video_test.mk
@@ -3,26 +3,26 @@
 # V4L2 video test application
 #
 #############################################################
-V4L2_VIDEOTEST_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-test/v4l2_video_test
-V4L2_VIDEOTEST_SITE_METHOD = svn
-V4L2_VIDEOTEST_VERSION = HEAD
+V4L2_VIDEO_TEST_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-test/v4l2_video_test
+V4L2_VIDEO_TEST_SITE_METHOD = svn
+V4L2_VIDEO_TEST_VERSION = HEAD
 
-define V4L2_VIDEOTEST_BUILD_CMDS
+define V4L2_VIDEO_TEST_BUILD_CMDS
 	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
 endef
 
-define V4L2_VIDEOTEST_INSTALL_TARGET_CMDS
+define V4L2_VIDEO_TEST_INSTALL_TARGET_CMDS
 	if ! [ -d "$(TARGET_DIR)/bin/" ]; then \
 		mkdir -p $(TARGET_DIR)/bin/; \
 	fi
 	$(INSTALL) -D -m 0755 $(@D)/v4l2_video_test $(TARGET_DIR)/bin/
 endef
 
-define V4L2_VIDEOTEST_CLEAN_CMDS
+define V4L2_VIDEO_TEST_CLEAN_CMDS
 	$(MAKE) -C $(@D) clean
 endef
 
-define V4L2_VIDEOTEST_UNINSTALL_TARGET_CMDS
+define V4L2_VIDEO_TEST_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/bin/v4l2_video_test
 endef
 
