3dda19bb879f37881b927b4bf057d4f501face6f
Sonic Zhang <sonic.zhang@analog.com>
2012-06-26 13:25:41 +0800

    bonnie++: Enlarge stack size for FLAT binary.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/bonnie/bonnie.mk
+++ package/bonnie/bonnie.mk
@@ -5,6 +5,7 @@
 BONNIE_VERSION = 1.94
 BONNIE_SOURCE = bonnie++-$(BONNIE_VERSION).tgz
 BONNIE_SITE = http://www.coker.com.au/bonnie++/experimental/
+BONNIE_CONF_ENV = LDFLAGS="$(TARGET_LDFLAGS) -Wl,-elf2flt=-s64000"
 
 define BONNIE_INSTALL_TARGET_CMDS
 	install -D -m 755 $(@D)/bonnie++ $(TARGET_DIR)/usr/sbin/bonnie++
