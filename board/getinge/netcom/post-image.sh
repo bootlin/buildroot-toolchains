7acd5e6ad6c21b5925f748d7181407bc08f18398
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-07-20 16:16:24 +0200

    netcom: append the build id number to the firmware image file name
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/getinge/netcom/post-image.sh
+++ board/getinge/netcom/post-image.sh
@@ -4,12 +4,16 @@ BINARIES_DIR=$1
 SWNUM=$(. board/getinge/netcom/rootfs-additions/usr/share/release/variables ; \
     echo $SWNUM)
 
+# The build ID file has been created or updated by the post-build
+# script executed previously
+BUILD_ID=$(cat .netcom_build_id)
+
 HWID=0x2424
 
-${BINARIES_DIR}/../host/usr/bin/fwupgrade-tool \
-    -o ${BINARIES_DIR}/${SWNUM}.bin           \
-    -p kernel:${BINARIES_DIR}/uImage           \
-    -p rootfs:${BINARIES_DIR}/rootfs.jffs2     \
+${BINARIES_DIR}/../host/usr/bin/fwupgrade-tool  \
+    -o ${BINARIES_DIR}/${SWNUM}_${BUILD_ID}.bin \
+    -p kernel:${BINARIES_DIR}/uImage            \
+    -p rootfs:${BINARIES_DIR}/rootfs.jffs2      \
     -i ${HWID}
 
 # Generate production files
