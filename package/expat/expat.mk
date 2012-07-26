f60555855eb4515c10348de386c2eb78da372d6a
Sonic Zhang <sonic.zhang@analog.com>
2012-07-26 15:04:31 +0800

    expat: Enable dynamic lib only when BR2_PREFER_STATIC_LIB is not set.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -8,24 +8,21 @@ EXPAT_VERSION = 2.0.1
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.gz
 EXPAT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/expat
 EXPAT_INSTALL_STAGING = YES
-ifeq ($(BR2_ABI_FLAT),y)
-EXPAT_INSTALL_TARGET = NO
-else
-EXPAT_INSTALL_TARGET = YES
-EXPAT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) installlib
-EXPAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) installlib
-endif
-
-EXPAT_CONF_OPT = --enable-shared
-
 EXPAT_DEPENDENCIES = host-pkg-config
 
-ifeq ($(BR2_ABI_FLAT),y)
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+EXPAT_INSTALL_TARGET = NO
+
 define EXPAT_INSTALL_STAGING_CMDS
 	install -D -m 755 $(@D)/.libs/libexpat.a $(STAGING_DIR)/usr/lib/libexpat.a
 	install -D -m 755 $(@D)/lib/expat.h $(STAGING_DIR)/usr/include/expat.h
 	install -D -m 755 $(@D)/lib/expat_external.h $(STAGING_DIR)/usr/include/expat_external.h
 endef
+else
+EXPAT_INSTALL_TARGET = YES
+EXPAT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) installlib
+EXPAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) installlib
+EXPAT_CONF_OPT = --enable-shared
 endif
 
 
