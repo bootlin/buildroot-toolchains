6e659782d2ead573b51774044fdfd6701d5e243e
Sonic Zhang <sonic.zhang@analog.com>
2012-07-26 16:09:32 +0800

    sqlite: Enable shared only when BR2_PREFER_STATIC_LIB is set.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -26,7 +26,7 @@ SQLITE_CONF_OPT = \
 	--enable-threadsafe \
 	--localstatedir=/var
 
-ifeq ($(BR2_ABI_FLAT),y)
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
 SQLITE_CONF_OPT += --enable-dynamic-extensions=no
 endif
 ifeq ($(BR2_PACKAGE_SQLITE_READLINE),y)
