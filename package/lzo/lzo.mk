24d099c893fd25ba7e3d38e8a7a9cdac4eb82c32
Sonic Zhang <sonic.zhang@analog.com>
2012-08-01 19:18:13 +0800

    lzo: Enalble shared only when BR2_PREFER_STATIC_LIB is not set.
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    
    Conflicts:
    
    	package/lzo/lzo.mk
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -7,7 +7,7 @@ LZO_VERSION = 2.06
 LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_INSTALL_STAGING = YES
 
-ifneq ($(BR2_ABI_FLAT),y)
+ifneq ($(BR2_PREFER_STATIC_LIB),y)
 LZO_CONF_OPT = --enable-shared
 endif
 
