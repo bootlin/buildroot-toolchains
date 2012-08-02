2285f287f8556fe6029381598877e7025d44a1a8
Sonic Zhang <sonic.zhang@analog.com>
2012-08-02 16:04:32 +0800

    lzo: remove shared config flags
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -7,9 +7,5 @@ LZO_VERSION = 2.06
 LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_INSTALL_STAGING = YES
 
-ifneq ($(BR2_PREFER_STATIC_LIB),y)
-LZO_CONF_OPT = --enable-shared
-endif
-
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
