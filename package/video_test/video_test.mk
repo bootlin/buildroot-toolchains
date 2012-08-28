aafb1ab7e3bad145dafa7440f7c67d8c42e86cde
Vivi Li <vivi.li@analog.com>
2012-08-28 16:33:11 +0800

    buildroot: package: add video test in package
    Signed-off-by: Vivi Li <vivi.li@analog.com>

---
+++ package/video_test/video_test.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# video test application
+#
+#############################################################
+VIDEO_TEST_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-test/video_test
+VIDEO_TEST_SITE_METHOD = svn
+VIDEO_TEST_VERSION = HEAD
+
+define VIDEO_TEST_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define VIDEO_TEST_INSTALL_TARGET_CMDS
+	if ! [ -d "$(TARGET_DIR)/bin/" ]; then \
+		mkdir -p $(TARGET_DIR)/bin/; \
+	fi
+	$(INSTALL) -D -m 0755 $(@D)/video_test $(TARGET_DIR)/bin/
+endef
+
+define VIDEO_TEST_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define VIDEO_TEST_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/bin/video_test
+endef
+
+$(eval $(call GENTARGETS))
