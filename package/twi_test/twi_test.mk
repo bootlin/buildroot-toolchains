e4bceb29332d3d3269c275ad6961dc313313bec9
Grace Pan <grace.pan@analog.com>
2012-08-31 11:24:07 +0800

    [#6684] Update script: fix twi_test makefile.
    Signed-off-by: Grace Pan <grace.pan@analog.com>

--- package/twi_test/twi_test.mk
+++ package/twi_test/twi_test.mk
@@ -7,16 +7,15 @@ TWI_TEST_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/
 TWI_TEST_SITE_METHOD = svn
 TWI_TEST_VERSION = HEAD
 
-
 define TWI_TEST_BUILD_CMDS
-	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+	$(MAKE) -C $(@D) BASE_DIR=$(BASE_DIR) MODULE_DIR=$(BASE_DIR)/build/twi_test-$(TWI_TEST_VERSION)  
 endef
 
 define TWI_TEST_INSTALL_TARGET_CMDS
 	if ! [ -d "$(TARGET_DIR)/bin/" ]; then \
 		mkdir -p $(TARGET_DIR)/bin/; \
 	fi
-	$(INSTALL) -D -m 0755 $(@D)/twi $(TARGET_DIR)/bin/
+	$(INSTALL) -D -m 0755 $(@D)/twi_test $(TARGET_DIR)/bin/
 endef
 
 define TWI_TEST_CLEAN_CMDS
@@ -24,7 +23,16 @@ define TWI_TEST_CLEAN_CMDS
 endef
 
 define TWI_TEST_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/bin/twi
+	rm -f $(TARGET_DIR)/bin/twi_test
 endef
 
 $(eval $(call GENTARGETS))
+
+
+$(BUILD_DIR)/twi_test-$(TWI_TEST_VERSION)/.stamp_extracted:
+	@$(call MESSAGE,"Extracting")
+	$(Q)cd $(@D)/../
+	$(Q)$(TAR) zxf $(DL_DIR)/$($(PKG)_SOURCE) -C $(@D)/../ 
+	$(Q)cp $($(PKG)_DIR_PREFIX)/twi_test/Makefile $(@D)/
+	$(Q)touch $@
+
