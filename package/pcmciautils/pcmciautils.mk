bc4b6aafe5ecb2c50a9af64efbf82cf059c32f0a
Steven Miao <realmz6@gmail.com>
2012-09-06 18:41:17 +0800

    [#7298] add pcmciautils for buildroot
    Signed-off-by: Steven Miao <realmz6@gmail.com>

---
+++ package/pcmciautils/pcmciautils.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# pcmciautils
+#
+#############################################################
+PCMCIAUTILS_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/pcmciautils/pcmciautils-015
+PCMCIAUTILS_SITE_METHOD = svn
+PCMCIAUTILS_VERSION = HEAD
+
+PCMCIAUTILS_DEPENDENCIES = libsysfs
+
+define PCMCIAUTILS_BUILD_CMDS
+	$(MAKE1) -C $(@D) OPTIMIZATION="$(TARGET_CFLAGS)" CROSS="$(TARGET_CROSS)"
+endef
+
+define PCMCIAUTILS_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define PCMCIAUTILS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/pccardctl $(TARGET_DIR)/sbin/
+	ln -s $(TARGET_DIR)/sbin/pccardctl $(TARGET_DIR)/sbin/lspcmcia
+endef
+
+define PCMCIAUTILS_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/sbin/pccardctl
+	rm -f $(TARGET_DIR)/sbin/lspcmcia
+endef
+
+$(eval $(call GENTARGETS))
