421ecba912a6316dc9a7fc6ca18987e4c4f5c1de
Bob Liu <lliubbo@gmail.com>
2012-09-11 14:32:16 +0800

    buildroot: package: add gnuplot
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

---
+++ package/gnuplot/gnuplot.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# gnuplot
+#
+#############################################################
+GNUPLOT_VERSION = 4.6.0
+GNUPLOT_SOURCE = gnuplot-$(GNUPLOT_VERSION).tar.gz
+GNUPLOT_SITE = http://superb-dca2.dl.sourceforge.net/project/gnuplot/gnuplot/$(GNUPLOT_VERSION)
+GNUPLOT_INSTALL_STAGING = YES
+GNUPLOT_INSTALL_TARGET = YES
+GNUPLOT_CONF_OPT = --without-demo --disable-demo
+
+$(eval $(generic-package))
