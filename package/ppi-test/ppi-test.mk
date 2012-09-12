581dd4291df3c10ef355a31290ac66980dda5d5f
Vivi Li <vivi.li@analog.com>
2012-09-12 16:36:34 +0800

    package: ppi-test: port application ppi-test from uclinux-dist
    Signed-off-by: Vivi Li <vivi.li@analog.com>

---
+++ package/ppi-test/ppi-test.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# bfin ppi test
+#
+#############################################################
+PPI_TEST_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-test/ppi-test
+PPI_TEST_SITE_METHOD = svn
+PPI_TEST_VERSION = HEAD
+
+define PPI_TEST_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define PPI_TEST_INSTALL_TARGET_CMDS
+	if ! [ -d "$(TARGET_DIR)/bin/" ]; then \
+		mkdir -p $(TARGET_DIR)/bin/; \
+	fi
+	$(INSTALL) -D -m 0755 $(@D)/ppitest $(TARGET_DIR)/bin/
+endef
+
+define PPI_TEST_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define PPI_TEST_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/bin/ppitest
+endef
+
+$(eval $(generic-package))
