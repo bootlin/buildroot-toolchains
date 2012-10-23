e7e38bc5eff506d1b41069a533a2f11adbdce654
Steven Miao <realmz6@gmail.com>
2012-10-23 17:00:51 +0800

    add old version thttpd, bfin canned demo will select old version thttpd
    Signed-off-by: Steven Miao <realmz6@gmail.com>

---
+++ package/thttpd-old/thttpd-old.mk
@@ -0,0 +1,36 @@
+#############################################################
+#
+# thttpd old
+#
+#############################################################
+
+THTTPD_OLD_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/blkfin-apps/ndso/thttpd
+THTTPD_OLD_SITE_METHOD = svn
+THTTPD_OLD_VERSION = HEAD
+
+THTTPD_OLD_MAKE_CFLAGS = $(TARGET_CFLAGS) -DHAVE_FCNTL_H=1 -DEMBED=1 -DHAVE_MEMORY_H=1 \
+        -DHAVE_PATHS_H=1 -DTIME_WITH_SYS_TIME=1 -DHAVE_DIRENT_H=1 \
+	        -DHAVE_LIBCRYPT=1 -DHAVE_STRERROR=1 -DHAVE_WAITPID=1 \
+		        -DHAVE_UNISTD_H=1 -DHAVE_GETPAGESIZE=1 -O2
+
+
+define THTTPD_OLD_BUILD_CMDS
+        $(MAKE) -C $(@D) CC="$(TARGET_CC)" CFLAGS="$(THTTPD_OLD_MAKE_CFLAGS)"
+endef
+
+define THTTPD_OLD_CLEAN_CMDS
+        $(MAKE) -C $(@D) clean
+endef
+
+define THTTPD_OLD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/thttpd $(TARGET_DIR)/sbin/
+endef
+
+define THTTPD_OLD_UNINSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/sbin/thttpd
+endef
+
+
+
+
+$(eval $(generic-package))
