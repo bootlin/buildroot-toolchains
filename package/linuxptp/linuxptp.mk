a79249c10946701ab2cec2b6af633ad4368e6b73
Bob Liu <lliubbo@gmail.com>
2012-11-13 18:25:53 +0800

    buildroot: linuxptp: add testptp
    Add testptp to linuxptp
    
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- package/linuxptp/linuxptp.mk
+++ package/linuxptp/linuxptp.mk
@@ -9,10 +9,13 @@ LINUXPTP_SITE_METHOD = git
 
 define LINUXPTP_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D)
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -o $(TOPDIR)/linux/linux-kernel/Documentation/ptp/testptp \
+		-lrt $(TOPDIR)/linux/linux-kernel/Documentation/ptp/testptp.c
 endef
 
 define LINUXPTP_INSTALL_TARGET_CMDS
 	cp -dpf $(@D)/ptp4l $(@D)/pmc $(@D)/phc2sys $(@D)/hwstamp_ctl $(TARGET_DIR)/usr/sbin
+	cp -dpf $(TOPDIR)/linux/linux-kernel/Documentation/ptp/testptp $(TARGET_DIR)/usr/sbin
 endef
 
 $(eval $(generic-package))
