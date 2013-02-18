373f191c86c142e29a4401b213b3272cb050b463
Steven Miao <realmz6@gmail.com>
2013-02-18 15:34:40 +0800

    strace: add FLT format stack size ENV
    Signed-off-by: Steven Miao <realmz6@gmail.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -8,12 +8,16 @@ STRACE_VERSION = HEAD
 STRACE_SITE_METHOD = svn
 STRACE_SITE = svn://firewall-sources.blackfin.uclinux.org:80/svn/uclinux-dist/trunk/user/strace
 
-FLTFLAGS += -s 0x5000
+ifeq ($(BR2_ABI_FLAT),y)
+STRACE_FLT_CONF_ENV = LDFLAGS="$(TARGET_LDFLAGS) -Wl,-elf2flt=-s0x5000"
+endif
+
 
 STRACE_CONF_ENV = ac_cv_header_linux_if_packet_h=yes \
 		  ac_cv_header_linux_netlink_h=yes \
 		  ac_cv_have_long_long_off_t=no \
-	          $(if $(BR2_LARGEFILE),ac_cv_type_stat64=yes,ac_cv_type_stat64=no)
+		  $(if $(BR2_LARGEFILE),ac_cv_type_stat64=yes,ac_cv_type_stat64=no) \
+			$(STRACE_FLT_CONF_ENV)
 
 STRACE_SUBDIR = strace-4.6
 
