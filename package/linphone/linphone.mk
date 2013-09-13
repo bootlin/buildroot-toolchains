bb1297f26f3ecb0d17ae01eba1c811398e3616e7
Sonic Zhang <sonic.zhang@analog.com>
2013-09-13 16:19:15 +0800

    bug[#108] linphone: stick linphone vertion to 3.3.2 for Blackfin
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- package/linphone/linphone.mk
+++ package/linphone/linphone.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-LINPHONE_VERSION = 3.5.2
-LINPHONE_SITE = http://download-mirror.savannah.gnu.org/releases/linphone/3.5.x/sources/
+LINPHONE_VERSION = 3.3.2
+LINPHONE_SITE = http://download-mirror.savannah.gnu.org/releases/linphone/3.3.x/sources/
 LINPHONE_CONF_OPT = \
-	--enable-external-ortp \
-	--enable-external-mediastreamer \
-	--disable-strict
+	--enable-fast-install
 
 LINPHONE_DEPENDENCIES = host-pkgconf ortp mediastreamer libeXosip2 speex
 
