1e19bc5ac6eaa640bbb8197f30c01d2b4d1a593b
Chris Wardman <cjwfirmware@gmail.com>
2015-03-31 21:29:37 -0400

    openocd update for 0.8

--- package/openocd/openocd.mk
+++ package/openocd/openocd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENOCD_VERSION = 0.5.0
+OPENOCD_VERSION = 0.8.0
 OPENOCD_SOURCE = openocd-$(OPENOCD_VERSION).tar.bz2
 OPENOCD_SITE = http://downloads.sourceforge.net/project/openocd/openocd/$(OPENOCD_VERSION)
 OPENOCD_LICENSE = GPLv2+
@@ -21,26 +21,41 @@ OPENOCD_DEPENDENCIES = libusb-compat
 
 # Adapters
 ifeq ($(BR2_PACKAGE_OPENOCD_FT2XXX),y)
-OPENOCD_CONF_OPTS += --enable-ft2232_libftdi
+OPENOCD_CONF_OPTS += --enable-ftdi
 OPENOCD_DEPENDENCIES += libftdi
 endif
 
-ifeq ($(BR2_PACKAGE_OPENOCD_JLINK),y)
-OPENOCD_CONF_OPTS += --enable-jlink
-endif
-
-ifeq ($(BR2_PACKAGE_OPENOCD_VSLLINK),y)
-OPENOCD_CONF_OPTS += --enable-vsllink
-endif
 
 HOST_OPENOCD_DEPENDENCIES = host-libusb-compat host-libftdi
 
-HOST_OPENOCD_CONF_OPTS = 	\
-	--disable-doxygen-html 	\
-	--enable-dummy 		\
-	--enable-ft2232_libftdi \
-	--enable-jlink 		\
-	--enable-vsllink
+#HOST_OPENOCD_CONF_OPTS = 	\
+#	--disable-doxygen-html 	\
+#	--enable-dummy 		\
+#	--enable-ftdi	
+
+HOST_OPENOCD_CONF_OPTS = --enable-ftdi --disable-doxygen-html
+
+define HOST_OPENOCD_CONFIGURE_CMDS
+	(cd $(@D); \
+		LDFLAGS="$(HOST_LDFLAGS)" \
+		CFLAGS="$(HOST_CFLAGS)" \
+		./configure \
+			--prefix=$(HOST_DIR)/usr \
+			--sysconfdir=$(HOST_DIR)/etc \
+			--localstatedir=$(HOST_DIR)/var \
+			--disable-gtk-doc \
+			--disable-doc \
+			--disable-docs \
+			--disable-documentation \
+			--disable-debug \
+			--with-xmlto=no \
+			--with-fop=no \
+			--disable-dependency-tracking \
+			$(HOST_OPENOCD_CONF_OPTS) \
+	)
+endef
+
+
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
