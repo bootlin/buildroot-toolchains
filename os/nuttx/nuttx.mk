2aa94df36a874cb104f3af37b9e63d51c6ae8540
Chris Wardman <cjwfirmware@gmail.com>
2015-04-20 04:05:03 -0400

    nuttx yartos support

--- os/nuttx/nuttx.mk
+++ os/nuttx/nuttx.mk
@@ -1,20 +1,21 @@
-NUTTX_VERSION = master
-NUTTX_SITE = git@github.com:vxmdesign/vtos.git
+NUTTX_VERSION = elena
+NUTTX_SITE = git@github.com:vxmdesign/yartos.git
 NUTTX_SITE_METHOD = git
 NUTTX_LICENSE = GPLv2+
 
 NUTTX_DEPENDENCIES = host-gcc-final
 
 define NUTTX_CONFIGURE_CMDS
-	echo "est"
+	$(MAKE) -C $(@D) TARGET_BOARD=$(BR2_NUTTX_TARGET_BOARD) $(BR2_NUTTX_DEFCONFIG) 
 endef
 
 define NUTTX_BUILD_CMDS
-	$(Q) $(MAKE) -C $(@D)
+	$(MAKE) -C $(@D) CROSSDEV=$(TARGET_CROSS) ARCROSSDEV=$(TARGET_CROSS) TOPDIR=$(@D)
 endef
 
 define NUTTX_INSTALL_TARGET_CMDS
-	echo "tes"
+	echo $(BINARIES_DIR)
+	install $(@D)/nuttx $(BINARIES_DIR)/nuttx 
 endef
 
 $(eval $(generic-package))
