68610818c24e60c6efeeee4156bb3b4ed24f6332
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-07-01 16:49:58 +0200

    sunxi-tools: use last commit to get the "fel spl" command
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- package/sunxi-tools/sunxi-tools.mk
+++ package/sunxi-tools/sunxi-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SUNXI_TOOLS_VERSION = v1.2
+SUNXI_TOOLS_VERSION = b80e7ce
 SUNXI_TOOLS_SITE = $(call github,linux-sunxi,sunxi-tools,$(SUNXI_TOOLS_VERSION))
 SUNXI_TOOLS_LICENSE = GPLv2+
 SUNXI_TOOLS_LICENSE_FILES = COPYING
